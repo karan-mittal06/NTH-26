a04f369c2d7c54ae712f9069631235928dee8717
Anushree Kamath <anushreekamath1@gmail.com>
2026-02-03 16:18:29 +0530

    Updated responsiveness of setters page. Upgraded next.js to latest version.

--- frontend/proxy.js
+++ frontend/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function middleware(request) {;
+export async function proxy(request) {;
   const tokenCookie = request.cookies.get('token')?.value;
   // console.log("token: ",tokenCookie);
   if (!tokenCookie) {
