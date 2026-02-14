29eec9f9ab86cce0e77e7d87a74020ea6e33bb44
karan mittal <mittalkaran572@gmail.com>
2026-02-14 16:05:48 +0530

    trying to add speed insights and stuff

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ToastUtils } from "@/utils/toastifyContainer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Network Treasure Hunt",
