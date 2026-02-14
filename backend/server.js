3f86fc293b29fe5871f9bc4a838d9a553c1b03a8
karan mittal <mittalkaran572@gmail.com>
2026-02-14 17:26:29 +0530

    change to testing

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.set('trust proxy', true);
-app.use(cors({origin:["https://nth.credenz.co.in"],credentials: true}));
+app.use(cors({origin:["https://nthtesting.credenz.co.in"],credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use((req,res,next)=>{
